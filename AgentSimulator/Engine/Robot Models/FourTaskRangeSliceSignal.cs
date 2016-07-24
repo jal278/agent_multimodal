d67311009a7b9c7ba2e1b0c91fe0230cbd372388
schrum2 <schrum2@utexas.edu>
2016-07-24 12:59:03 -0500

    New Robot Model
    Fixed problem with robot model. The four tasks domain requires a robot
    that supports a signal sensor that is used in the team patrol
    environment. However, this sensor is not desired in the Dual Task or Two
    Rooms domains. The simple solution (though not great in the long run
    because of code duplication) was to create a new robot model that
    basically copies the MazeRobotPieSlice model, except it adds a signal
    sensor.
    
    Remaining problem: Pie slice sensors activate when they shouldn't.
    Specifically, in the team patrol, lone patrol, and dual task hallway
    environments. This last one may actually have been a problem in our
    original experiments, but not a huge issue.

---
+++ AgentSimulator/Engine/Robot Models/FourTaskRangeSliceSignal.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using simulator.Sensor_Models;
+
+/**
+ *  This code duplicates the MazeRobotPieSlice,
+ *  but adds a signal sensor to the robot so it will
+ *  also work in the team patrol task. Not great
+ *  code ... too much code is copied.
+ **/
+namespace Engine
+{
+    class FourTaskRangeSliceSignal : Khepera3RobotModelContinuous
+    {
+        private PieSliceSensorArray pieSliceSensor;
+
+        public FourTaskRangeSliceSignal()
+            : base()
+        {
+            name = "FourTaskRangeSliceSignal";
+            default_speed = 25.0f;
+            default_turn_speed = 9.0f;
+            actualRange = 40.0f;
+            collisionAvoidance = false;
+            autopilot = false;
+            addtimer = false;
+        }
+
+        public override float defaultRobotSize()
+        {
+            return 10.5f;
+        }
+
+        public override int defaultSensorDensity()
+        {
+            return 5;
+        }
+        
+        public override void populateSensors(int numRangeFinders)
+        {
+            base.populateSensors(numRangeFinders);
+        }
+        
+
+        public override void populateSensors()
+        {
+            base.populateSensors(); // Only adds range finders
+
+            //Adds the pieSlice sensor to the robot
+            pieSliceSensor = new PieSliceSensorArray(this);
+
+            // Schrum: This one line distinguishes this code from MazeRobotPieSlice
+            sensors.Add(new SignalSensor(this));
+        }
+
+        public override void updateSensors(Environment env, List<Robot> robots, CollisionManager cm)
+        {
+            base.updateSensors(env, robots, cm);
+            pieSliceSensor.update(env, robots, cm);
+        }
+
+        protected override void decideAction(float[] outputs, float timeStep)
+        {
+            float speed = 20.0f;
+            float turnSpeed = 4.28f; ;
+
+            velocity = speed * outputs[1];
+            heading += (outputs[2] - outputs[0]) * turnSpeed * timeStep;
+        }
+
+    }
+}
