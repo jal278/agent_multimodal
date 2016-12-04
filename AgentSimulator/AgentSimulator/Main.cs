e914e6d505dfed56ccb299288dec8b0ce33d48fe
Jacob Schrum <schrum2@southwestern.edu>
2016-12-03 20:03:59 -0600

    Changed default experiment to match new file names

--- AgentSimulator/AgentSimulator/Main.cs
+++ AgentSimulator/AgentSimulator/Main.cs
@@ -350,7 +350,7 @@ namespace PackbotExperiment
             }
             else
             {
-                experimentName = "FourTasks-EXP-1M.xml"; //"adversary_two_room_EXP.xml"; //"EXP_dual_task.xml";//"hardmaze_exp.xml";//EXP_dual_task.xml";      //Default experiment 
+                experimentName = "EXP_FourTasks-avg-1M.xml"; //"adversary_two_room_EXP.xml"; //"EXP_dual_task.xml";//"hardmaze_exp.xml";//EXP_dual_task.xml";      //Default experiment 
                 //"hardmaze_exp.xml";//
 
                 for (int j = 0; j < args.Length; j++)
